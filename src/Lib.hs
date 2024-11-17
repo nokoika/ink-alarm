b25a0fa6ee0ba10dbab8350de2c9e822c89ccfbb
nokoika <mrfool38+noko_ika@gmail.com>
2024-11-17 21:32:22 +0900

    JSON のデコードサンプル

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,18 @@
 module Lib (generateICalEvents, someFunc) where
 
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base64.URL as B64U (decode)
+import qualified Data.ByteString.Base64.URL as BU (decode)
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T (Text, pack)
 import qualified Data.Text.Encoding as TE (decodeUtf8', encodeUtf8)
 import qualified Data.Text.Lazy as TL (fromStrict)
 import qualified ICal (ICalEvent (..), Reminder (..), ReminderAction (..), ReminderTimeUnit (..), ReminderTrigger (..), buildICalText)
 import qualified Web.Scotty as S (ActionM, get, queryParam, scotty, text)
 import Prelude (Bool (False), Either (Left, Right), Eq, IO, Int, Maybe (Just, Nothing), Show, String, ($), (.), (<>), (>>=))
-import qualified Prelude as P (putStrLn, return, show)
+import qualified Prelude as P (putStrLn, return, show, print)
+import Data.Aeson (FromJSON, decode)
+import GHC.Generics (Generic)
+
 
 -- SplatoonStageScheduleQueryを表すデータ型
 data SplatoonStageScheduleQuery = SplatoonStageScheduleQuery
@@ -113,16 +117,33 @@ hoge = do
   events <- generateICalEvents query
   P.putStrLn $ ICal.buildICalText events (language query)
 
-decodeBase64UriToJson :: BS.ByteString -> Either String T.Text
-decodeBase64UriToJson base64Uri = case B64U.decode base64Uri of
-  Left err -> Left $ P.show err
-  Right decoded -> case TE.decodeUtf8' decoded of
-    Left err -> Left $ P.show err
-    Right decodedText -> Right decodedText
-
-someFunc =
+fuga =
   S.scotty 3000 $
     S.get "/decode" $
       (S.queryParam "data" :: S.ActionM T.Text) >>= \base64Uri -> S.text $ case decodeBase64UriToJson $ TE.encodeUtf8 base64Uri of
         Left err -> TL.fromStrict $ T.pack err
         Right decodedText -> TL.fromStrict decodedText
+
+decodeBase64UriToJson :: BS.ByteString -> Either String T.Text
+decodeBase64UriToJson base64Uri = case BU.decode base64Uri of
+  Left err -> Left $ P.show err
+  Right decoded -> case TE.decodeUtf8' decoded of
+    Left err -> Left $ P.show err
+    Right decodedText -> Right decodedText
+
+data Person = Person
+  { name :: String,
+    age :: Int
+  }
+  deriving (Show, Generic)
+instance FromJSON Person
+
+someFunc = do
+  let jsonString :: T.Text
+      jsonString = "{\"name\":\"Charlie\",\"age\":28}"
+
+  -- Text を ByteString に変換してから decode する
+  let person = decode (BL.fromStrict $ TE.encodeUtf8 jsonString) :: Maybe Person
+  case person of
+    Just p  -> P.print p
+    Nothing -> P.putStrLn "Failed to parse JSON"
