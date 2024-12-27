0ae88097ad9b6a41f65d2288a4e9bc4b44207d3a
nokoika <mrfool38+noko_ika@gmail.com>
2024-12-27 22:37:53 +0900

    メタタグ用に複数枚htmlを用意

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,4 +6,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        ja: 'ja.html',
+        en: 'en.html',
+      },
+    },
+  },
 });
