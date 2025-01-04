6cbaacf80d7a33ec37acc86e2fbd7d8812620570
nokoika <mrfool38+noko_ika@gmail.com>
2025-01-04 15:32:38 +0900

    パッケージ不足解消

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -57,11 +57,11 @@ const InputBlock: FC<{
   return (
     <div className="grid gap-4 border-4 px-4 py-3 border-nord-1">
       <div className="flex items-center gap-2">
-        <div className="text-nord-10">
+        <div className="text-nord-10 bg-nord-1 p-2 rounded-bl-xl rounded-tr-xl">
           <Icon />
         </div>
         <h2 className="text-lg font-semibold text-nord-6">{title}</h2>
-        <div className="text-nord-10">
+        <div className="text-nord-10 bg-nord-1 p-2 rounded-bl-xl rounded-tr-xl">
           <Icon />
         </div>
       </div>
